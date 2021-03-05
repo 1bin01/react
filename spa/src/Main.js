a4f1235b2056e728043cda82747b1e2ce496c920
1bin01 <wonbin0504@naver.com>
2021-03-05 16:38:43 +0900

    이제 class 안씀..;;

--- spa/src/Main.js
+++ spa/src/Main.js
@@ -7,16 +7,16 @@ class Main extends Component{
     render(){
         return (<HashRouter>
              <div>
-                <h1>SPA</h1>
+                <h1 >SPA</h1>
                 <ul className="header">
-                    <li><NavLink to="/1"> <Button secondary size = 'big'>home </Button></NavLink></li>
-                    <li><NavLink to="/2"> <Button secondary size = 'big'>stuff </Button></NavLink></li>
-                    <li><NavLink to="/3"> <Button secondary size = 'big'>contact </Button></NavLink></li>
-                    <li><NavLink to="/4"> <Button secondary size = 'big'>about </Button></NavLink></li>
+                    <li><NavLink to="/home"> <Button secondary size = 'big'>home </Button></NavLink></li>
+                    <li><NavLink to="/stuff"> <Button secondary size = 'big'>stuff </Button></NavLink></li>
+                    <li><NavLink to="/contact"> <Button secondary size = 'big'>contact </Button></NavLink></li>
+                    <li><NavLink to="/about"> <Button secondary size = 'big'>about </Button></NavLink></li>
                     
                 </ul>
                 <div className = "content">
-                    <Route path= "/1" component={Home}></Route>
+                    <Route path= "/" component={Home}></Route>
                 </div>
             </div>
         </HashRouter>
